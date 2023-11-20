6be823211873a648e3b1bdec8cf2a31d4ffa973f
chrlyz <chrlyz@skiff.com>
2023-11-20 08:31:58 +0000

    Implement verification of Auro Wallet SignedData and CORS to receive requests from the NextJS client

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
 import fastify from 'fastify';
-import { createFileEncoderStream, CAREncoderStream } from 'ipfs-car';
-import { CircuitString, PublicKey, Signature } from 'o1js';
+import { CircuitString, PublicKey, Signature} from 'o1js';
+import { SignedData } from '@aurowallet/mina-provider';
+import cors from '@fastify/cors'
 
 // ============================================================================
 
 const server = fastify();
+await server.register(cors, { 
+  origin: 'http://localhost:3000',
+  methods: ['POST']
+});
 
-server.post<{Body: CreatePost}>('/', async (request, reply) => {
-  const isSigned = await validatePost(
-    request.body.posterAddress,
-    request.body.postContent,
-    request.body.signature
-    );
+server.post<{Body: SignedData}>('/posts*', async (request, reply) => {
+
+  const signatureJSON = {
+    r: request.body.signature.field,
+    s: request.body.signature.scalar
+  }
+
+  const signature = Signature.fromJSON(signatureJSON);
+
+  const isSigned = signature.verify(
+    PublicKey.fromBase58(request.body.publicKey), 
+    [CircuitString.fromString(request.body.data).hash()]
+  )
   
   if (isSigned) {
     return request.body;
@@ -20,52 +32,12 @@ server.post<{Body: CreatePost}>('/', async (request, reply) => {
   }
 });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 3001 }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
   console.log(`Server listening at ${address}`)
 });
-
-// ============================================================================
-
-interface CreatePost {
-  posterAddress: string;
-  postContent: string;
-  signature: string;
-}
-
-// ============================================================================
-
-async function validatePost(
-  posterAddressBase58: string,
-  postContent: string,
-  signatureBase58: string
-  ) {
-  const posterAddress =  PublicKey.fromBase58(posterAddressBase58);
-  const signature = Signature.fromBase58(signatureBase58);
-
-  let postCID: any;
-  const file = new Blob([postContent]);
-  await createFileEncoderStream(file)
-  .pipeThrough(
-    new TransformStream({
-      transform(block, controller) {
-        postCID = block.cid;
-        controller.enqueue(block);
-      },
-    })
-  )
-  .pipeThrough(new CAREncoderStream())
-  .pipeTo(new WritableStream());
-
-  const isSigned = signature.verify(
-    posterAddress,
-    [CircuitString.fromString(postCID.toString()).hash()]
-  );
-
-  return isSigned.toBoolean();
-}
 
 // ============================================================================
