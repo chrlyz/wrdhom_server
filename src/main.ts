b580da76e67c46d4385d6a17ad7aba1820f9ddc0
chrlyz <chrlyz@skiff.com>
2023-11-21 04:01:45 +0000

    Implement PostgreSQL with Prisma for persistent storage of posts data

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import fastify from 'fastify';
-import { CircuitString, PublicKey, Signature} from 'o1js';
+import { CircuitString, PublicKey, Signature, fetchLastBlock} from 'o1js';
 import { SignedData } from '@aurowallet/mina-provider';
-import cors from '@fastify/cors'
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
 // ============================================================================
 
@@ -10,6 +11,8 @@ await server.register(cors, {
   origin: 'http://localhost:3000',
   methods: ['POST']
 });
+
+const prisma = new PrismaClient();
 
 server.post<{Body: SignedData}>('/posts*', async (request, reply) => {
 
@@ -26,6 +29,32 @@ server.post<{Body: SignedData}>('/posts*', async (request, reply) => {
   )
   
   if (isSigned) {
+    const allpostscounter = await prisma.posts.count();
+
+    const userpostsCID = await prisma.posts.findMany({
+      where: {
+        posteraddress: request.body.publicKey
+      },
+      select: {
+        postcontentid: true
+      }
+    });
+    const userpostscounter = userpostsCID.length;
+
+    const lastBlock = await fetchLastBlock('https://proxy.berkeley.minaexplorer.com/graphql');
+    const postblockheight = lastBlock.blockchainLength.toBigint();
+
+    await prisma.posts.create({
+      data: {
+        posteraddress: request.body.publicKey,
+        postcontentid: request.body.data,
+        allpostscounter: (allpostscounter + 1),
+        userpostscounter: (userpostscounter + 1),
+        postblockheight: postblockheight,
+        deletionblockheight: 0
+      }
+    })
+
     return request.body;
   } else {
     reply.code(401).send({error: `Post isn't signed`});
