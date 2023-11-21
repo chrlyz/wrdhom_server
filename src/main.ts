0ff63c86588f53caee6bc2df9d8af96c6d9590a1
chrlyz <chrlyz@skiff.com>
2023-11-21 05:24:25 +0000

    Get post content and verify that it matches the signed post CID

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { CircuitString, PublicKey, Signature, fetchLastBlock} from 'o1js';
 import { SignedData } from '@aurowallet/mina-provider';
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
+import { createFileEncoderStream, CAREncoderStream } from 'ipfs-car';
+import { Blob } from '@web-std/file';
 
 // ============================================================================
 
@@ -14,50 +16,51 @@ await server.register(cors, {
 
 const prisma = new PrismaClient();
 
-server.post<{Body: SignedData}>('/posts*', async (request, reply) => {
+server.post<{Body: SignedPost}>('/posts*', async (request, reply) => {
 
   const signatureJSON = {
-    r: request.body.signature.field,
-    s: request.body.signature.scalar
+    r: request.body.signedData.signature.field,
+    s: request.body.signedData.signature.scalar
   }
 
   const signature = Signature.fromJSON(signatureJSON);
+  const posterAddress = request.body.signedData.publicKey;
+  const postContentID = request.body.signedData.data;
 
-  const isSigned = signature.verify(
-    PublicKey.fromBase58(request.body.publicKey), 
-    [CircuitString.fromString(request.body.data).hash()]
-  )
-  
-  if (isSigned) {
-    const allpostscounter = await prisma.posts.count();
-
-    const userpostsCID = await prisma.posts.findMany({
-      where: {
-        posteraddress: request.body.publicKey
+  const file = new Blob([request.body.post]);
+  let postCID: any;
+  await createFileEncoderStream(file)
+  .pipeThrough(
+  new TransformStream({
+      transform(block, controller) {
+      postCID = block.cid;
+      controller.enqueue(block);
       },
-      select: {
-        postcontentid: true
-      }
-    });
-    const userpostscounter = userpostsCID.length;
+  })
+  )
+  .pipeThrough(new CAREncoderStream())
+  .pipeTo(new WritableStream());
 
-    const lastBlock = await fetchLastBlock('https://proxy.berkeley.minaexplorer.com/graphql');
-    const postblockheight = lastBlock.blockchainLength.toBigint();
+  if (postCID.toString() === postContentID) {
+    console.log(postCID.toString());
+    console.log(postContentID);
+    const isSigned = signature.verify(
+      PublicKey.fromBase58(posterAddress), 
+      [CircuitString.fromString(postContentID).hash()]
+    );
+    
+    if (isSigned) {
 
-    await prisma.posts.create({
-      data: {
-        posteraddress: request.body.publicKey,
-        postcontentid: request.body.data,
-        allpostscounter: (allpostscounter + 1),
-        userpostscounter: (userpostscounter + 1),
-        postblockheight: postblockheight,
-        deletionblockheight: 0
-      }
-    })
+      await createSQLPost(posterAddress, postContentID);
 
-    return request.body;
+      return request.body;
+    } else {
+        reply.code(401).send({error: `Post isn't signed`});
+    }
   } else {
-    reply.code(401).send({error: `Post isn't signed`});
+      reply.code(401).send({error: `Derived post CID, doesn't match signed post CID`});
+      console.log(postCID.toString());
+      console.log(postContentID);
   }
 });
 
@@ -70,3 +73,36 @@ server.listen({ port: 3001 }, (err, address) => {
 });
 
 // ============================================================================
+
+interface SignedPost {
+  post: string,
+  signedData: SignedData
+}
+
+// ============================================================================
+
+const createSQLPost = async (posterAddress: string, postContentID: string) => {
+  const allpostscounter = await prisma.posts.count();
+  const userpostsCID = await prisma.posts.findMany({
+    where: {
+      posteraddress: posterAddress
+    },
+    select: {
+      postcontentid: true
+    }
+  });
+  const userpostscounter = userpostsCID.length;
+  const lastBlock = await fetchLastBlock('https://proxy.berkeley.minaexplorer.com/graphql');
+  const postblockheight = lastBlock.blockchainLength.toBigint();
+
+  await prisma.posts.create({
+    data: {
+      posteraddress: posterAddress,
+      postcontentid: postContentID,
+      allpostscounter: (allpostscounter + 1),
+      userpostscounter: (userpostscounter + 1),
+      postblockheight: postblockheight,
+      deletionblockheight: 0
+    }
+  })
+}
