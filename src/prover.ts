cc1d5050168ff263493cc8d2852416ce3d370493
chrlyz <chrlyz@skiff.com>
2023-12-10 02:56:14 +0000

    Wrap state regeneration from database logic on utility function

--- src/prover.ts
+++ src/prover.ts
@@ -8,6 +8,7 @@ import { performance } from 'perf_hooks';
 import { PrismaClient } from '@prisma/client';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { regenerateZkAppState } from './utils/state.js';
 
 // ============================================================================
 
@@ -50,68 +51,18 @@ console.log(`${(endTime - startTime)/1000/60} minutes`);
 
 // Regenerate Merkle maps from database
 
-let usersPostsCountersMap = new MerkleMap();
-let postsMap = new MerkleMap();
+const usersPostsCountersMap = new MerkleMap();
+const postsMap = new MerkleMap();
+let numberOfPosts = 0;
 
-const posts = await prisma.posts.findMany({
-  orderBy: {
-    allPostsCounter: 'asc'
-  },
-  where: {
-    postBlockHeight: {
-      not: 0
-    }
-  }
-});
-console.log('posts:');
-console.log(posts)
+const context = {
+  prisma: prisma,
+  usersPostsCountersMap: usersPostsCountersMap,
+  postsMap: postsMap,
+  numberOfPosts: numberOfPosts
+}
 
-const posters = new Set(posts.map( post => post.posterAddress));
-console.log('posters:');
-console.log(posters);
-
-for (const poster of posters) {
-const userPosts = await prisma.posts.findMany({
-  where: { posterAddress: poster,
-  postBlockHeight: {
-  not: 0
-  } },
-  select: {
-    userPostsCounter: true
-  }
-});
-console.log('Initial usersPostsCountersMap root: ' + usersPostsCountersMap.getRoot().toString());
-usersPostsCountersMap.set(
-  Poseidon.hash(PublicKey.fromBase58(poster).toFields()),
-  Field(userPosts.length)
-);
-console.log(userPosts.length);
-console.log('Latest usersPostsCountersMap root: ' + usersPostsCountersMap.getRoot().toString());
-};
-
-posts.forEach( post => {
-  const posterAddress = PublicKey.fromBase58(post.posterAddress);
-  const posterAddressAsField = Poseidon.hash(posterAddress.toFields());
-  const postContentID = CircuitString.fromString(post.postContentID);
-  const postState = new PostState({
-    posterAddress: posterAddress,
-    postContentID: postContentID,
-    allPostsCounter: Field(post.allPostsCounter),
-    userPostsCounter: Field(post.userPostsCounter),
-    postBlockHeight: Field(post.postBlockHeight),
-    deletionBlockHeight: Field(post.deletionBlockHeight),
-    restorationBlockHeight: Field(post.restorationBlockHeight)
-  });
-  console.log('Initial postsMap root: ' + postsMap.getRoot().toString());
-  postsMap.set(
-    Poseidon.hash([posterAddressAsField, postContentID.hash()]),
-    postState.hash()
-  );
-  console.log('Latest postsMap root: ' + postsMap.getRoot().toString());
-});
-
-let numberOfPosts = posts.length;
-console.log('Original number of posts: ' + numberOfPosts);
+await regenerateZkAppState(context);
 
 while (true) {
   // Process pending posts to update on-chain state
